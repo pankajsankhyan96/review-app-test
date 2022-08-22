f845345f72695395c4d54dc73ec844e8f24e850c
manish negi <manish.negi@dipoletechi.com>
2022-08-22 18:07:45 +0530

    fsd

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 var http = require('http')
 var express = require('express')
 var app = express()
+require('../config');
 var port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.end("Hello, This is my first node project, running on Port : " + port + "  Environment : " + process.env.NODE_ENV );
+    res.end("Hello, This is my first node project, running on Port : " + port + "  Environment : " + process.env.NODE_ENV + " dbhost: " + process.env.DB_HOST );
 })
 
 app.listen(port, () => {
