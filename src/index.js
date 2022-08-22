2dcfd1e8da117e148b9c971e6832b13640c8fe50
manish negi <manish.negi@dipoletechi.com>
2022-08-22 17:59:16 +0530

    sdfsdf

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var http = require('http')
 var express = require('express')
 var app = express()
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.end("Hello, This is my first node project, running on Port : " + port + "  Environment : " + process.env.NODE_ENV );
 })
